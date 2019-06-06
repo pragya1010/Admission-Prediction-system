a87d5d2fa946232408bebf662372a7b60f4bc0f0
Pragya Moharatha <pragya@Pragyas-MacBook-Pro.local>
2019-06-06 16:34:48 -0700

    Added Coefficients of Regression

--- project.py
+++ project.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn import tree, linear_model
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import os
 import seaborn as sns
 sns.set(style='white')
 sns.set(style='whitegrid', color_codes=True)
 import warnings
 warnings.filterwarnings("ignore")
+from scipy.stats import linregress
 
 
 class Admission_Predictor:
@@ -52,7 +53,10 @@ class Admission_Predictor:
 
         # Considering the best correalations features
         main_features = ['CGPA', 'GRE Score', 'TOEFL Score']
-        for i in range (len(main_features)):
+        for i in range(len(main_features)):
+            print(main_features[i].upper())
+            print(linregress(self.data[main_features[i]], self.data['Chance of Admit ']))
+            
             plt.figure(figsize=(20, 6))
             plt.subplot(1, 2, 1)
             sns.distplot(self.data[main_features[i]])
@@ -63,6 +67,7 @@ class Admission_Predictor:
             plt.title(main_features[i] + ' vs Chance of Admit')
             plt.savefig(main_features[i] +'.pdf')
 
+        
 
         # Bar Plots
         df = self.data
@@ -94,7 +99,10 @@ class Admission_Predictor:
         plt.show()
 
 
+
+
     def model_decision(self):
+
         # data Pre - processing
         cols = self.data.columns
         features = cols[1:-1]
@@ -109,10 +117,9 @@ class Admission_Predictor:
         train_y = y[:int(0.75*len(X))]
         test_y = y[int(0.75*len(X)):]
 
-        # methods = ['Linear Regression', 'Decision Tree Regression', 'Decision Tree Classifier']
 
         # Decision tree Regression
-        self.model = DecisionTreeRegressor(min_samples_leaf=10,random_state=0,max_leaf_nodes=20)
+        self.model = DecisionTreeRegressor(max_leaf_nodes=10)
         self.model.fit(train_X, train_y)
 
 
@@ -120,9 +127,6 @@ class Admission_Predictor:
         self.model2 = linear_model.LinearRegression()
         self.model2.fit(train_X, train_y)
 
-        # # Classifier Model
-        # self.model3 = DecisionTreeClassifier()
-        # self.model3.fit(train_X,train_y)
 
         print(self.model)
         print(self.model2)
@@ -139,34 +143,51 @@ class Admission_Predictor:
         self.score1 = self.model.score(test_X,test_y)
         print("Score1: ", self.score1)
 
-        self.predicted_2 = self.model.predict(test_X)
+        self.predicted_2 = self.model2.predict(test_X)
         self.predicted_full_2 = self.model2.predict(X)
         self.score2 = self.model2.score(test_X, test_y)
         print("Score2: ", self.score2)
+        print("Coefficients-----------", self.model2.coef_)
+        print("Intercept-----------", self.model2.intercept_)
 
-        # self.predicted_3 = self.model3.predict(test_X)
-        # self.predicted_full_3 = self.model3.predict(X)
-        # score3 = self.model3.score(test_X, test_y)
-        # print("Score3: ", score3)
+
+
 
         return train_X, test_X, train_y, test_y
 
 
     # Actual - predicted for test/train data
     def error_calc(self, test):
-        mae = mean_absolute_error(test, self.predicted)
-        print("Mean Absolute error: ", mae)
+        mae_DR = mean_absolute_error(test, self.predicted)
+        mse_DR = mean_squared_error(test, self.predicted)
+        r2_DR  = r2_score(test, self.predicted)
+
+        mae_LR = mean_absolute_error(test, self.predicted_2)
+        mse_LR = mean_squared_error(test, self.predicted_2)
+        r2_LR = r2_score(test, self.predicted_2)
+
+
+        print("Errors - Linear Regression: \n Mean Absolute Error: {} \n  Mean Squared Error: {} \n R2 Score: {}".format(mae_LR, mse_LR, r2_LR))
+        print("Errors - Decision Tree Regression: \n Mean Absolute Error: {} \n  Mean Squared Error: {} \n R2 Score: {}".format(mae_DR, mse_DR, r2_DR))
+        
 
     def output_results(self):
         df1 = pd.DataFrame()
-        df1['predictions'] = self.predicted_full
-        df1['predictions_2'] = self.predicted_full_2
+        df1['predictions_DR'] = self.predicted_full
+        df1['predictions_LR'] = self.predicted_full_2
         final_df = pd.merge(left=self.data, right=df1, left_index=True, right_index=True)
         final_df['True_Decision'] = final_df['Chance of Admit '].apply(lambda x: "Yes" if x > 0.80 else "No")
-        final_df['Decision'] = final_df['predictions'].apply(lambda x: "Yes" if x > 0.80 else "No")
-        # print(final_df)
+        final_df['Decision_DR'] = final_df['predictions_DR'].apply(lambda x: "Yes" if x > 0.80 else "No")
+        final_df['Decision_LR'] = final_df['predictions_LR'].apply(lambda x: "Yes" if x > 0.80 else "No")
+        print(final_df[['GRE Score','TOEFL Score','University Rating','SOP','LOR ','CGPA','Research','Chance of Admit ','predictions_LR']].tail(5))
+        print(final_df[['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR ', 'CGPA', 'Research',
+                        'Chance of Admit ', 'predictions_DR']].tail(5))
 
-        final_df.to_csv('Acceptance_Prediction1.csv', index=False)
+        # final_df.to_csv('Acceptance_Prediction1.csv', index=False)
+        # plt.plot(final_df.index, final_df['predictions_LR'], color='red', label='Predicted')
+        # plt.plot(final_df.index, final_df['Chance of Admit '], color='blue', label='Actual')
+        # plt.legend()
+        # plt.show()
 
 
 ad = Admission_Predictor()
